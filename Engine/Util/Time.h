8a0031e1fe727e66231ebb2d6aaad0960ebc9799
WangYuHan <1269940762@qq.com>
2024-01-10 17:00:59 +0800

    this is a TimeManager class  .

---
+++ Engine/Util/Time.h
@@ -0,0 +1,15 @@
+#pragma once
+namespace Engine
+{
+	class Time
+	{
+	public:
+		static float deltaTime;
+		static float timeSinceBegining;
+		static unsigned long long frame;
+
+		static void update(double time);
+	};
+}
+
+
